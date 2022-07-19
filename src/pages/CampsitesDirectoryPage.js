57ea1a36fa10ccdef3031d40f25eb39d31bea76f
Zachary <Zachlipscomb86@gmail.com>
2022-07-19 06:13:24 -0700

    Breadcrumbs subheadeer

--- src/pages/CampsitesDirectoryPage.js
+++ src/pages/CampsitesDirectoryPage.js
@@ -1,9 +1,13 @@
 import { Container } from 'reactstrap';
 import CampsitesList from '../features/campsites/CampsitesList';
+import SubHeader from '../components/SubHeader';
 
 const CampsitesDirectoryPage = () => {
+
+
     return (
         <Container>
+            <SubHeader current='home' detail={true} />
             <CampsitesList />
         </Container>
     );
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,11 @@
 import { Container } from 'reactstrap';
 import DisplayList from '../features/display/DisplayList';
+import SubHeader from '../components/SubHeader';
 
 const HomePage = () => {
     return (
         <Container>
+            <SubHeader current='home' detail={true} />
             <DisplayList />
         </Container>
     );
