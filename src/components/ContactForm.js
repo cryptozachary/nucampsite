122ec232e28e83738f05666a59fff58882133ba9
Zachary <Zachlipscomb86@gmail.com>
2022-07-27 13:56:13 -0700

    Contact Form Part 3

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,7 +1,13 @@
-import { Button, Label, Col, FormGroup } from 'reactstrap'
-import { Formik, Field, Form } from 'formik'
+import { Button, Label, Col, FormGroup } from 'reactstrap';
+import { Formik, Field, Form } from 'formik';
 
-export default function ContactForm() {
+const ContactForm = () => {
+    const handleSubmit = (values, { resetForm }) => {
+        console.log('form values:', values);
+        console.log('in JSON format:', JSON.stringify(values));
+        resetForm();
+    };
+
     return (
         <Formik
             initialValues={{
@@ -11,44 +17,62 @@ export default function ContactForm() {
                 email: '',
                 agree: false,
                 contactType: 'By Phone',
-                feedback: ''
+                feedback: '',
             }}
+            onSubmit={handleSubmit}
         >
             <Form>
                 <FormGroup row>
-                    <Label htmlFor='firstName' md='2' >
+                    <Label htmlFor='firstName' md='2'>
                         First Name
                     </Label>
                     <Col md='10'>
-                        <Field className='form-control' name='firstName' placeholder='First Name' />
+                        <Field
+                            name='firstName'
+                            placeholder='First Name'
+                            className='form-control'
+                        />
                     </Col>
                 </FormGroup>
                 <FormGroup row>
-                    <Label htmlFor='lastName' md='2' >
+                    <Label htmlFor='lastName' md='2'>
                         Last Name
                     </Label>
                     <Col md='10'>
-                        <Field className='form-control' name='lastName' placeholder='Last Name' />
+                        <Field
+                            name='lastName'
+                            placeholder='Last Name'
+                            className='form-control'
+                        />
                     </Col>
                 </FormGroup>
                 <FormGroup row>
-                    <Label htmlFor='phoneNum' md='2' >
+                    <Label htmlFor='phoneNum' md='2'>
                         Phone
                     </Label>
                     <Col md='10'>
-                        <Field className='form-control' name='phoneNum' placeholder='Phone' />
+                        <Field
+                            name='phoneNum'
+                            placeholder='Phone'
+                            className='form-control'
+                        />
                     </Col>
                 </FormGroup>
                 <FormGroup row>
-                    <Label htmlFor='email' md="2" >
+                    <Label htmlFor='email' md='2'>
                         Email
                     </Label>
                     <Col md='10'>
-                        <Field className='form-control' md='2' name='email' placeholder='Email' />
+                        <Field
+                            name='email'
+                            placeholder='Email'
+                            type='email'
+                            className='form-control'
+                        />
                     </Col>
                 </FormGroup>
                 <FormGroup row>
-                    <Label md={{ size: 4, offset: 2 }}>
+                    <Label check md={{ size: 4, offset: 2 }}>
                         <Field
                             name='agree'
                             type='checkbox'
@@ -57,7 +81,11 @@ export default function ContactForm() {
                         May we contact you?
                     </Label>
                     <Col md='4'>
-                        <Field className='form-control' name='contactType' as='select'>
+                        <Field
+                            name='contactType'
+                            as='select'
+                            className='form-control'
+                        >
                             <option>By Phone</option>
                             <option>By Email</option>
                         </Field>
@@ -68,13 +96,24 @@ export default function ContactForm() {
                         Your Feedback
                     </Label>
                     <Col md='10'>
-                        <Field className='form-control' name='feedback' as='textarea' rows='12' />
+                        <Field
+                            name='feedback'
+                            as='textarea'
+                            rows='12'
+                            className='form-control'
+                        />
                     </Col>
                 </FormGroup>
                 <FormGroup row>
+                    <Col md={{ size: 10, offset: 2 }}>
+                        <Button type='submit' color='primary'>
+                            Send Feedback
+                        </Button>
+                    </Col>
                 </FormGroup>
             </Form>
-
         </Formik>
-    )
-}
+    );
+};
+
+export default ContactForm;
