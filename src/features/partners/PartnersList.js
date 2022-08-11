09488780b38dbaded8fd31c1d92c96d2a1319094
Zachary <Zachlipscomb86@gmail.com>
2022-08-11 14:07:05 -0700

    Async Partners

--- src/features/partners/PartnersList.js
+++ src/features/partners/PartnersList.js
@@ -2,23 +2,33 @@ import { Col, Row } from 'reactstrap'
 import Partner from './Partner'
 import { selectAllPartners } from "./partnersSlice"
 import { useSelector } from 'react-redux/es/hooks/useSelector'
-
+import Error from '../../components/Error'
+import Loading from '../../components/Loading'
 
 const PartnersList = () => {
 
+    const isLoading = useSelector((state) => state.partners.isLoading);
+    const errMsg = useSelector((state) => state.partners.errMsg);
+
     const partners = useSelector(selectAllPartners);
 
-    return (
+    return isLoading ? (
+        <Loading />
+    ) : errMsg ? (
+        <Error errMsg={errMsg} />
+    ) : (
         <Col className='mt-4'>
             <Row>
                 {partners.map((partner) => {
-                    return <div className='d-flex mb-5' key={partner.id}>
-                        <Partner partner={partner} />
-                    </div>
+                    return (
+                        <div className='d-flex mb-5' key={partner.id}>
+                            <Partner partner={partner} />
+                        </div>
+                    );
                 })}
             </Row>
         </Col>
-    )
+    );
 }
 
 export default PartnersList
